216de38af40ca544eba3640880edd5528837ee17
Carlos Emanuel Silva e Melo Oliveira <carlos.olvr32@gmail.com>
2024-08-22 09:42:06 -0300

    Converter resposta de bytes para json

--- LAB01-graphQL/main.py
+++ LAB01-graphQL/main.py
@@ -1,11 +1,12 @@
 import requests
+import json
 
 url = "https://api.github.com/graphql"
 token = "token"
 headers = {"Authorization" : f"token {token}"}
 body = """
 query {
-  search(query: "topic:Open Source sort:stars-desc", type: REPOSITORY, first: 10) {
+  search(query: "topic:Open Source sort:stars-desc", type: REPOSITORY, first: 1) {
     edges {
       node {
         ... on Repository {
@@ -48,8 +49,8 @@ query {
 """
 
 response = requests.post(url=url, headers=headers, json={"query" : body})
-print(response.status_code)
 if response.status_code == 200:
-    print("reponse : ", response.content)
+    json_response = json.loads(response.content.decode('utf-8'))
+    print(json_response)
 else:
     raise Exception(f"Falha na requisição para o repositório: {response.status_code}")

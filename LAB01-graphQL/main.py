9eed7efc626c17004494b6169a7558f965e0aa96
Carlos Emanuel Silva e Melo Oliveira <carlos.olvr32@gmail.com>
2024-08-25 15:47:17 -0300

    Arquivo .csv com 1000 repositórios

--- LAB01-graphQL/main.py
+++ LAB01-graphQL/main.py
@@ -12,8 +12,8 @@ BASE_URL = "https://api.github.com/graphql"
 ACCESS_TOKEN = os.getenv("TOKEN") 
 REQUEST_HEADERS = {"Authorization" : f"token {ACCESS_TOKEN}"}
 QUERY_STRING = """
-query($query: String!, $num: Int, $cursor: String) {
-  search(query: $query, type: REPOSITORY, first: $num, after: $cursor ) {
+query($query: String!, $cursor: String) {
+  search(query: $query, type: REPOSITORY, first: 20, after: $cursor ) {
     edges {
       node {
         ... on Repository {
@@ -51,13 +51,16 @@ query($query: String!, $num: Int, $cursor: String) {
         }
       }
     }
+    pageInfo{
+        endCursor
+    }
   }
 }
 """
 list_of_dict = []
 
-def get_repos(query, num):
-    variables = {'query': f'{query} sort:stars-desc', 'num': num, 'cursor': ""}
+def get_repos(query, cursor):
+    variables = {'query': f'{query} sort:stars-desc', 'cursor': cursor}
     response = requests.post(url=BASE_URL, headers=REQUEST_HEADERS, json={"query" : QUERY_STRING, "variables" : variables})
     if response.status_code == 200:
         json_response = json.loads(response.content.decode('utf-8'))
@@ -65,26 +68,31 @@ def get_repos(query, num):
     else:
         raise Exception(f"Falha na requisição para o repositório: {response.status_code}")
 
-def create_dict(json_response):
-    list_repo = json_response["data"]["search"]["edges"]
+def get_info(query, num):
+    cursor = ""
+    while len(list_of_dict) < num:
 
-    for repo in list_repo:
-        node = repo["node"]
-        this_repo = {'name': {node["name"]}, 
-                     'create_date': {node["createdAt"]}, 
-                     'total_pull_requests': {node["pullRequests"]["totalCount"]},
-                     'total_releases': {node["releases"]["totalCount"]}, 
-                     'last_commit_date': {node["defaultBranchRef"]["target"]["committedDate"]},
-                     'main_language': {'N/A' if node["primaryLanguage"] == None else node["primaryLanguage"]["name"]}, 
-                     'total_issues': {node["issues"]["totalCount"]}, 
-                     'total_stars': {node["stargazerCount"]}}
+        json_response = get_repos(query,cursor)
+        list_repo = json_response["data"]["search"]["edges"]
+
+        for repo in list_repo:
+            node = repo["node"]
+            this_repo = {'name': {node["name"]}, 
+                        'create_date': {node["createdAt"]}, 
+                        'total_pull_requests': {node["pullRequests"]["totalCount"]},
+                        'total_releases': {node["releases"]["totalCount"]}, 
+                        'last_commit_date': {node["defaultBranchRef"]["target"]["committedDate"]},
+                        'main_language': {'N/A' if node["primaryLanguage"] == None else node["primaryLanguage"]["name"]}, 
+                        'total_issues': {node["issues"]["totalCount"]}, 
+                        'total_stars': {node["stargazerCount"]}}
 
         list_of_dict.append(this_repo)
+        cursor = json_response["data"]["search"]["pageInfo"]["endCursor"]
 
     return list_of_dict
 
 def save_to_csv(repos_info):
-    filename = "repos.csv"
+    filename = "repos1.csv"
     fields = ['name', 'create_date', 'total_pull_requests', 'total_releases', 'last_commit_date', 'main_language', 'total_issues', 'total_stars']
 
     with open(filename, 'w') as csvfile:
@@ -98,6 +106,5 @@ if __name__ == "__main__":
     elif len(sys.argv) > 3:
         print("Too many arguments")
     else:
-        response = get_repos(sys.argv[1], int(sys.argv[2]))
-        repos_info = create_dict(response)
+        repos_info = get_info(sys.argv[1], int(sys.argv[2]))
         save_to_csv(repos_info)

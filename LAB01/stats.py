1ae43c09d919e8eeaa3407f84c31c7f50028ecb0
Carlos Emanuel Silva e Melo Oliveira <carlos.olvr32@gmail.com>
2024-09-01 21:33:03 -0300

    Mudando formato da data no arquivo .csv

--- LAB01/stats.py
+++ LAB01/stats.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 def read_data_from_csv(path):
     data = pd.read_csv(path)
-    return data
+    data['create_date'] = pd.to_datetime(data['create_date']).dt.year
+    data.to_csv('new_repos_file.csv', index=False)
 
 if __name__ == "__main__":
-    data = read_data_from_csv("repos.csv")
-    print(data)
+    read_data_from_csv("repos.csv")
+    print('Alterado')
